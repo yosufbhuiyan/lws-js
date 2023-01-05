cdd5d6891489ddfa371444b2823725a85529a289
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-05 23:17:37 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,39 @@
+// var x = 12;
+// var y =11;
+// var z =x + y;
+
+// console.log(z);
+
+// let a ;
+// a = 22;
+
+// let b;
+// b = 22;
+
+// let c = a + b;
+
+// console.log(c);
+
+
+// {
+//     let a = 'x';
+//     console.log(a)
+// }
+
+
+// const a = 30;
+
+// //a = 20;
+// console.log(a);
+
+// carName = "Volvo";
+// console.log(carName);
+
+// var carName;
+
+
+carName = "BMW"
+
+console.log(carName);
+
+let carName;
